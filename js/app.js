68146d0236154032ca8c7614a78e95aec9ba64ec
raihan2bd <raihan2bd.official@gmail.com>
2022-11-20 13:29:07 +0600

    Add Header for the project

---
+++ js/app.js
@@ -0,0 +1,18 @@
+// Shortcut function for dom manupulation
+const gElem = (x)  => {return document.querySelector(x);}
+const gElemAll = (x)  => document.querySelectorAll(x);
+
+// Function for toggling class
+const toggleMnav = () => gElem('.m-nav').classList.toggle('df');
+
+// Showing navigaioon item using toggle class
+const menu = gElem('.menu-icon');
+menu.addEventListener('click', toggleMnav);
+
+// Add event listener on every nav;
+gElemAll('.m-nav .nav-link').forEach(item => {
+  item.addEventListener('click', toggleMnav);
+});
+
+// Clossing mobile menu using cross icon
+gElem('.cross-icon').addEventListener('click', toggleMnav)
